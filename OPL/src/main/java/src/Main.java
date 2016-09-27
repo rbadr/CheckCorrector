8fac35a1620d266d90ef3d6e91135b658534e601
Jihad <jihad394@hotmail.fr>
2016-09-27 15:14:38 +0200

    First Commit

---
+++ OPL/src/main/java/src/Main.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.util.Iterator;
+
+import spoon.Launcher;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtField;
+import spoon.reflect.factory.Factory;
+
+public class Main {
+	public static void main(String[] args) {
+		Factory factory=new Launcher().createFactory();
+		final CtClass<?> test=(CtClass<?>) factory.Type().get(Test.class);
+		Iterator<CtField<?>> methods=test.getFields().iterator();
+		while (methods.hasNext()){
+			CtField<?> method=methods.next();
+			System.out.println(method);
+		}
+		factory.Eval();
+	}
+}
---
+++ OPL/src/main/java/src/Test.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Test {
+	private String v;
+
+	public void Rien() {
+
+	}
+
+	public void test() {
+
+	}
+}
